d3f752095c4b8ddb813feec675e57f95b35bfae4
run10 <runningbreaker10k@gmail.com>
2025-10-05 20:12:00 +0900

    Del competitions

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -16,6 +16,7 @@ export default function robots(): MetadataRoute.Robots {
           '/mypage',
           '/login',
           '/signup',
+          '/competitions/',
         ],
       },
       {
@@ -29,6 +30,7 @@ export default function robots(): MetadataRoute.Robots {
           '/mypage',
           '/login',
           '/signup',
+          '/competitions/',
         ],
       },
       {
@@ -42,6 +44,7 @@ export default function robots(): MetadataRoute.Robots {
           '/mypage',
           '/login',
           '/signup',
+          '/competitions/',
         ],
       },
       {
@@ -55,6 +58,7 @@ export default function robots(): MetadataRoute.Robots {
           '/mypage',
           '/login',
           '/signup',
+          '/competitions/',
         ],
       },
     ],
